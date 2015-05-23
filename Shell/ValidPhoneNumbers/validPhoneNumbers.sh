01476e8f16ff36eac2cefb0b1028a4a2cd5d6c1c
Summon Yang <SummonYang1991@gmail.com>
2015-05-23 09:07:06 +0800

    add sed method.

--- Shell/ValidPhoneNumbers/validPhoneNumbers.sh
+++ Shell/ValidPhoneNumbers/validPhoneNumbers.sh
@@ -8,6 +8,10 @@ grep -P '^(\d{3}-|\(\d{3}\) )\d{3}-\d{4}$' file.txt
 echo "egrep:"
 egrep -o "^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$" file.txt
 
+# Using sed
+echo "sed:"
+sed -n -r '/^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$/p' file.txt
+
 # Using awk
 echo "awk:"
 awk '/^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$/' file.txt
